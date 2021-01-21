11f702464767f1c64dbfb1854487cdf760fb4c1d
folabi-masha <folabi.masha@gmail.com>
2021-01-21 10:31:31 -0500

    updated settings

--- LandellsBarbershop/settings.py
+++ LandellsBarbershop/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
-    'LandellsBarbershop',
-    'Landells'
+    'LandellsBarbershop'
 ]
 
 MIDDLEWARE = [
